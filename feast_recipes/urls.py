4f0dcf196231d7b32816ce3fe333bff772cbbab6
raj thakur <rajthakur913124@gmail.com>
2025-03-15 16:58:06 +0530

    commited change

--- feast_recipes/urls.py
+++ feast_recipes/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('search/', views.recipes, name='recipe_search'),
     path('upload/', views.upload_recipe, name='upload_recipe'),
     path('recipe/<int:recipe_id>/', views.recipe_content, name='recipe_content'),
-    path("like/<int:recipe_id>/", views.like_recipe, name="like_recipe"),
+    path("like_recipe/<int:recipe_id>/", views.like_recipe, name="like_recipe"),
     path("save/<int:recipe_id>/", views.save_recipe, name="save_recipe"),
     path('view/<int:recipe_id>/', views.increment_view_count, name='view_recipe'),
 ]
